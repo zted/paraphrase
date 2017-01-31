0de086d24587efba0f43dd065653be90e8531ce4
zted <tedz.cs@gmail.com>
2017-01-31 16:36:13 +0100

    Cleaned up paraphrase for review

--- paraphrase.py
+++ paraphrase.py
@@ -2,80 +2,45 @@ import numpy as np
 
 from keras.models import Sequential
 from keras.preprocessing.sequence import pad_sequences
-from keras.layers import Dense, Activation, LSTM, TimeDistributed, Embedding, RepeatVector
+from keras.layers import Dense, Activation, GRU, TimeDistributed, Embedding, RepeatVector
 from keras.callbacks import Callback
-from nltk import word_tokenize
 from nltk.corpus import brown
 
 
+maxlen = 20
+minlen = 3
+hidden_dim = 128
+embedding_dim = 100
+batch_size = 50
+num_epochs = 1
+
+
 def load_word_embeddings(fname):
-    idx = {}
+    # loads word embeddings into a dictionary
+    embDict = {}
     with open(fname, 'r') as f:
         for line in f:
             splits = line.strip().split(' ')
             word = splits[0]
             wordvec = np.array(map(float, splits[1:]))
-            idx[word] = wordvec
-    return idx
+            embDict[word] = wordvec
+    return embDict
 
 
-brownSents = brown.sents(categories=['hobbies', 'news', 'fiction', 'adventure', 'reviews', 'editorials', 'romance'])
-
-gloveDict = load_word_embeddings('glove.6B.100d.txt')
-maxlen = 20
-sentences = []
-words = set([])
-word2idx = {}
-hidden_dim = 128
-embedding_dim = 100
-batch_size = 50
-num_epochs = 20
-count = 0
-
-for s in brownSents:
-    skip = False
-    if len(s) <= maxlen:
-        lowered = [t.lower() for t in s]
-        for t in lowered:
-            if t not in words:
-                try:
-                    gloveDict[t]
-                    words.add(t)
-                    word2idx[t] = count
-                    count += 1
-                except KeyError as e:
-                    skip = True
-        if not skip:
-            sentVec = [word2idx[t] for t in lowered]
-            sentences.append(sentVec)
-
-
-num_words = len(word2idx)
-embeddingsMatrix = np.zeros((len(words), embedding_dim))
-
-
-for k, idx in word2idx.items():
-    embeddingsMatrix[idx, :] = word2idx[k]
-
-gloveDict = None
-
-
-def get_model(max_len, num_words, embedding_dim, hidden_dim, batch_size, embeddings=None):
+def build_model(max_len, num_words, embedding_dim, hidden_dim, embeddings=None):
     model = Sequential()
-    model.add(Embedding(input_dim=num_words,
+    model.add(Embedding(input_dim=num_words+1,
                         input_length=max_len,
-                        batch_input_shape=[batch_size, max_len],
-                        mask_zero=True,
                         output_dim=embedding_dim,
+                        mask_zero=True,
                         weights=embeddings))
-    model.add(LSTM(output_dim=hidden_dim,
-                   return_sequences=False,
-                   stateful=False))
+    model.add(GRU(output_dim=hidden_dim,
+                  input_length=max_len))
     model.add(RepeatVector(max_len))
-    model.add(LSTM(output_dim=hidden_dim,
-                   return_sequences=True,
-                   stateful=True))
-    model.add(TimeDistributed(Dense(num_words), input_shape=(max_len, hidden_dim)))
+    model.add(GRU(output_dim=hidden_dim,
+                  input_length=max_len,
+                  return_sequences=True))
+    model.add(TimeDistributed(Dense(num_words)))
     model.add(Activation("softmax"))
     model.compile(optimizer='rmsprop',
                   loss='categorical_crossentropy',
@@ -84,8 +49,8 @@ def get_model(max_len, num_words, embedding_dim, hidden_dim, batch_size, embeddi
 
 
 def generate_text(model, seed_words, temperature, word2idx, idx2word, N):
+    # keep this here for reference
     model.reset_states()
-    # first we initialize the state of the LSTM using the seed_str
     for seed_word in seed_words:
         seed_word_idx = word2idx[seed_word]
         x = np.zeros(shape=model.input_shape)
@@ -97,7 +62,6 @@ def generate_text(model, seed_words, temperature, word2idx, idx2word, N):
     next_word_idx = sample(probs, temperature)
     generated_text_idx = [next_word_idx]
     generated_text = [idx2word[next_word_idx]]
-    x = np.zeros(shape=model.input_shape)
     for i in xrange(N - 1):
         last_word_idx = generated_text_idx[-1]
         x = np.zeros(shape=model.input_shape)
@@ -110,8 +74,21 @@ def generate_text(model, seed_words, temperature, word2idx, idx2word, N):
     return generated_text
 
 
-class ResetStates(Callback):
+def generate_text2(model, inputs, idx2word):
+    model.reset_states()
+    # don't sample, just take the prediction with the highest probability
+    probs = model.predict(np.matrix(inputs), verbose=0)
+    generated_text = []
+    print probs
+    print probs.shape
+    for p in probs[0]:
+        idx = np.argmax(p) # -1 because we
+        generated_text.append(idx2word[idx])
+    return generated_text
 
+
+class ResetStates(Callback):
+    # is this callback necessary?
     def on_epoch_begin(self, epoch, logs={}):
         self.model.reset_states()
 
@@ -126,24 +103,73 @@ def sample(preds, temperature=1.0):
     return np.argmax(probas)
 
 
-model = get_model(maxlen, num_words, embedding_dim, hidden_dim, batch_size, embeddings=[embeddingsMatrix])
+brownSents = brown.sents(categories=['hobbies', 'news', 'fiction', 'adventure', 'reviews', 'editorials', 'romance'])
+print('Finished loading sentences from brown corpus')
+gloveDict = load_word_embeddings('glove.6B.100d.txt')
+print('Finished loading word embeddings')
+word2idx = {}
+sentences = []
+unique_words = set([])
+count = 1
+raw_sentences = []
+print('Vectorizing sentences')
+for s in brownSents:
+    skip = False
+    if len(s) <= maxlen and len(s) >= minlen:
+        # only take sentences that aren't too long nor too short
+        lowered = [t.lower() for t in s]
+        words_to_add = []
+        for w in lowered:
+            if w not in unique_words:
+                try:
+                    # see if we have a word embedding for this word, if not we skip
+                    gloveDict[w]
+                    words_to_add.append(w)
+                except KeyError as e:
+                    skip = True
+                    break
+        if not skip:
+            # all words in the sentence are in the embeddings, so we use this sentence
+            # vectorize it
+            raw_sentences.append(' '.join(lowered))
+            for w in set(words_to_add):
+                unique_words.add(w)
+                word2idx[w] = count
+                count += 1
+            sentVec = [word2idx[t] for t in lowered]
+            sentences.append(sentVec)
 
-print len(sentences)
-print num_words
+# we start at count = 1 because the input sequence is padded with 0's until a vector of 20 is reached
+# cannot use the index 0 for any words in the vocab
+num_words = len(word2idx)
+assert count-1 == num_words
+embeddingsMatrix = np.zeros([num_words+1, embedding_dim])
+# create embeddings matrix corresponding to the word indices
+
+print('Creating weights for embeddings layer')
+for word, idx in word2idx.items():
+    embeddingsMatrix[idx, :] = gloveDict[word]
+gloveDict = None
+
+print('Building model')
+model = build_model(maxlen, num_words, embedding_dim, hidden_dim, embeddings=[embeddingsMatrix])
 
 sentences_target = np.zeros(shape=(len(sentences), maxlen, num_words), dtype=bool)
+print('Creating target data')
 for ns, sent in enumerate(sentences):
     for nt, t in enumerate(sent):
-        sentences_target[ns, nt, t] = 1
+        sentences_target[ns, nt, t-1] = 1
+        # -1 because we don't use padding for output but index 0 is padding in input
 
-X = np.array(pad_sequences(sentences, maxlen=maxlen, padding='post'))[0:10000]
-Y = np.array(sentences_target)[0:10000]
+# train on the first 10000 data points
+X = pad_sequences(sentences, maxlen=maxlen, padding='post')[0:10000]
+Y = sentences_target[0:10000]
+model.fit(X, Y, batch_size=batch_size, nb_epoch=num_epochs)
 model_name = 'paraphrase.brown.h5'
-model.fit(X, Y, batch_size=batch_size, nb_epoch=num_epochs, callbacks=[ResetStates()])
 model.save_weights(model_name, overwrite=True)
 
-seed_str = 'as the river flows'
-seed_str = [w.lower() for w in word_tokenize(seed_str)]
+seed_str = list(X[0])
 model.load_weights(model_name)
-generated_text = generate_text(model, seed_str, 1.1, word2idx, words, 3000)
-print generated_text[0:10]
+generated_text = generate_text2(model, seed_str, list(unique_words))
+print('Original sentence:\n{}'.format(raw_sentences[0]))
+print('Paraphrased:\n{}'.format(' '.join(generated_text)))
