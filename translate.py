3230dbbc4a2805bcd12798d1b67eb5bebef179e8
zted <tedz.cs@gmail.com>
2017-02-04 23:07:24 +0100

    Can somewhat paraphrase

--- translate.py
+++ translate.py
@@ -12,13 +12,13 @@ from tensorflow.python.util import nest
 os.environ['TF_CPP_MIN_LOG_LEVEL']='3'
 
 max_examples = 10000
-maxlen = 17
+maxlen = 18
 minlen = 3
 hidden_dim = 128
 embedding_dim = 100
 batch_size = 50
-num_epochs = 1000
-validate_cycle = 100
+num_epochs = 2000
+validate_cycle = 50
 validation_size = 5
 
 
@@ -44,54 +44,6 @@ def load_word_embeddings(fname):
             embDict[word] = wordvec
     return embDict
 
-
-def generate_text(model, seed_words, temperature, word2idx, idx2word, N):
-    # keep this here for reference
-    model.reset_states()
-    for seed_word in seed_words:
-        seed_word_idx = word2idx[seed_word]
-        x = np.zeros(shape=model.input_shape)
-        x[0, 0] = seed_word_idx
-        probs = model.predict(x, verbose=0)
-
-    # now we start generating text
-    probs = probs[0, 0, :]
-    next_word_idx = sample(probs, temperature)
-    generated_text_idx = [next_word_idx]
-    generated_text = [idx2word[next_word_idx]]
-    for i in xrange(N - 1):
-        last_word_idx = generated_text_idx[-1]
-        x = np.zeros(shape=model.input_shape)
-        x[0, 0] = last_word_idx
-        probs = model.predict(x, verbose=0)
-        probs = probs[0, 0, :]
-        next_word_idx = sample(probs, temperature)
-        generated_text_idx.append(next_word_idx)
-        generated_text.append(idx2word[next_word_idx])
-    return generated_text
-
-
-def generate_text2(model, inputs, idx2word):
-    model.reset_states()
-    # don't sample, just take the prediction with the highest probability
-    probs = model.predict(np.matrix(inputs), verbose=0)
-    generated_text = []
-    print probs
-    print probs.shape
-    for p in probs[0]:
-        idx = np.argmax(p) # -1 because we
-        generated_text.append(idx2word[idx])
-    return generated_text
-
-
-def sample(preds, temperature=1.0):
-    # helper function to sample an index from a probability array
-    preds = np.asarray(preds).astype('float64')
-    preds = np.log(preds) / temperature
-    exp_preds = np.exp(preds)
-    preds = exp_preds / np.sum(exp_preds)
-    probas = np.random.multinomial(1, preds, 1)
-    return np.argmax(probas)
 
 def embedding_rnn_seq2seq2(encoder_inputs,
                           decoder_inputs,
@@ -160,15 +112,34 @@ def embedding_rnn_seq2seq2(encoder_inputs,
     return outputs_and_state[:outputs_len], state
 
 
+def train_batch(X, Y):
+    X = np.array(X).T
+    Y = np.array(Y).T
+    feed_dict = {enc_inp[t]: X[t] for t in range(seq_length)}
+    feed_dict.update({labels[t]: Y[t] for t in range(seq_length)})
+
+    _, loss_t, summary = sess.run([train_op, loss, summary_op], feed_dict)
+    return loss_t, summary
+
+
 brownSents = brown.sents(categories=['hobbies', 'news', 'fiction', 'adventure', 'reviews', 'editorials', 'romance'])
 print('Finished loading sentences from brown corpus')
 gloveDict = load_word_embeddings('glove.6B.100d.txt')
+gloveNum = len(gloveDict)
+embeddingsMatrix = np.zeros([gloveNum+1, embedding_dim])
+# create embeddings matrix corresponding to the word indices
+print('Creating weights for embeddings layer')
+
+word2idx = {'PAD':0}
+all_words = ['PAD']
+for n, (word, vec) in enumerate(gloveDict.items()):
+    word2idx[word] = n+1
+    embeddingsMatrix[n+1, :] = vec
+    all_words.append(word)
+gloveDict = None
+
 print('Finished loading word embeddings')
-word2idx = {}
 sentences = []
-unique_words = set(['NONE'])
-all_words = ['NONE']
-count = 1
 raw_sentences = []
 print('Vectorizing sentences')
 for s in brownSents:
@@ -176,50 +147,37 @@ for s in brownSents:
     if len(s) <= maxlen and len(s) >= minlen:
         # only take sentences that aren't too long nor too short
         lowered = [t.lower() for t in s]
-        words_to_add = []
+        sentVec = []
         for w in lowered:
-            if w not in unique_words:
-                try:
+            try:
                     # see if we have a word embedding for this word, if not we skip
-                    gloveDict[w]
-                    words_to_add.append(w)
-                except KeyError as e:
-                    skip = True
-                    break
+                idx = word2idx[w]
+                sentVec.append(idx)
+            except KeyError as e:
+                skip = True
+                break
         if not skip:
             # all words in the sentence are in the embeddings, so we use this sentence
             # vectorize it
             raw_sentences.append(' '.join(lowered))
-            for w in set(words_to_add):
-                unique_words.add(w)
-                all_words.append(w)
-                word2idx[w] = count
-                count += 1
-            sentVec = [word2idx[t] for t in lowered]
             sentences.append(sentVec)
             if len(sentences) >= max_examples:
                 # we have enough
                 break
 
+print('Number of training examples we have: {}\n'.format(len(sentences)))
 # we start at count = 1 because the input sequence is padded with 0's until a vector of 20 is reached
 # cannot use the index 0 for any words in the vocab
-num_words = len(unique_words)
-assert count == num_words
-assert count == len(all_words)
-embeddingsMatrix = np.zeros([num_words, embedding_dim])
-# create embeddings matrix corresponding to the word indices
-print('Creating weights for embeddings layer')
-for word, idx in word2idx.items():
-    embeddingsMatrix[idx, :] = gloveDict[word]
-word2idx['NONE'] = 0
-gloveDict = None
+# num_words = len(unique_words)
+# assert count == num_words
+# assert count == len(all_words)
 
 print('Building model')
 
 X = pad_sequences(sentences, maxlen=maxlen)
 
 seq_length = maxlen
-vocab_size = num_words
+vocab_size = len(word2idx)
 memory_dim = 100
 
 enc_inp = [tf.placeholder(tf.int32, shape=(None,),
@@ -241,7 +199,7 @@ dec_inp = ([tf.zeros_like(enc_inp[0], dtype=np.int32, name="GO")]
 # Initial memory value for recurrence.
 prev_mem = tf.zeros((batch_size, memory_dim))
 cell = tf.nn.rnn_cell.GRUCell(memory_dim)
-dec_outputs, dec_memory = embedding_rnn_seq2seq2(enc_inp, dec_inp, cell, vocab_size, vocab_size, embedding_dim, pretrained_embeddings=tf.constant_initializer(embeddingsMatrix))
+dec_outputs, dec_memory = embedding_rnn_seq2seq2(enc_inp, dec_inp, cell, vocab_size, vocab_size, embedding_dim, initializer=tf.constant_initializer(embeddingsMatrix))
 loss = tf.nn.seq2seq.sequence_loss(dec_outputs, labels, weights, vocab_size)
 tf.summary.scalar("loss", loss)
 magnitude = tf.sqrt(tf.reduce_sum(tf.square(dec_memory[1])))
@@ -254,22 +212,26 @@ train_op = optimizer.minimize(loss)
 sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True))
 sess.run(tf.global_variables_initializer())
 
-
-def train_batch(X, Y):
-    X = np.array(X).T
-    Y = np.array(Y).T
-    feed_dict = {enc_inp[t]: X[t] for t in range(seq_length)}
-    feed_dict.update({labels[t]: Y[t] for t in range(seq_length)})
-
-    _, loss_t, summary = sess.run([train_op, loss, summary_op], feed_dict)
-    return loss_t, summary
-
-
-allWords = all_words
 nb_batches = int(np.ceil(len(X) / float(batch_size)))
-
-print('Number of training examples we have: {}\nNumber of unique words: {}'.format(len(sentences),num_words-1))
+print('Number of training examples we have: {}\n'.format(len(sentences)))
 print('Begin training')
+
+
+def predict_unseen_sentence(sentence):
+    unseen_vectorized = [word2idx[w] for w in sentence.split(' ')]
+    vector_padded = np.array(pad_sequences([unseen_vectorized], maxlen))
+    X_test = np.array(vector_padded)
+    X_test = np.array(X_test).T
+    feed_dict = {enc_inp[t]: X_test[t] for t in range(seq_length)}
+    dec_outputs_batch = sess.run(dec_outputs, feed_dict)
+    answers = [logits_t.argmax(axis=1) for logits_t in dec_outputs_batch]
+    unseen_prediction = []
+    for a in answers:
+        unseen_prediction.append(all_words[a[0]])
+    print('\nUnseen Sentence:{}\nUnseen Prediction:{}'.format(sentence, ' '.join(unseen_prediction)))
+
+
+unseen_sent = ['defendants testifies after fears about election', 'i like taking walks', 'the dog is a great and caring animal']
 for t in range(num_epochs):
 
     for i in range(nb_batches):
@@ -292,23 +254,10 @@ for t in range(num_epochs):
         for sent in answers:
             sentString = []
             for token in sent:
-                sentString.append(allWords[token])
+                sentString.append(all_words[token])
             generated_text.append(' '.join(sentString))
 
         print('Original sentences:\n{}'.format([raw_sentences[v] for v in val_indices]))
         print(generated_text)
-
-
-unseen_sent = 'defendants testifies after fears about election'
-unseen_vectorized = [word2idx[w] for w in unseen_sent.split(' ')]
-vector_padded = np.array(pad_sequences([unseen_vectorized], maxlen))
-X_test = np.array(vector_padded)
-X_test = np.array(X_test).T
-feed_dict = {enc_inp[t]: X_test[t] for t in range(seq_length)}
-dec_outputs_batch = sess.run(dec_outputs, feed_dict)
-answers = [logits_t.argmax(axis=1) for logits_t in dec_outputs_batch]
-unseen_prediction = []
-for a in answers:
-    unseen_prediction.append(allWords[a[0]])
-print('\nUnseen Prediction:')
-print ' '.join(unseen_prediction)
+        for u in unseen_sent:
+            predict_unseen_sentence(u)
